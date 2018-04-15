1b57dae95a223ed47ffe96e78240c8adba124a65
Nick Boober <nboober@terpmail.umd.edu>
2018-04-15 13:06:34 -0400

    Removed unnecessary comments

--- js/app.js
+++ js/app.js
@@ -79,7 +79,7 @@ let secondSelectedClass;
     //This setTimeout delays the matching function for one second so the cards can flip completely
     setTimeout(function afterCardFlip(){
         matchingCards();
-      }, 2000);
+      }, 2500);
       }
     });
 
@@ -173,13 +173,3 @@ function win(){
     alert("You win!! \n You beat the game in " + scoreValue + " moves.");
     }
   }
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
